92ed14e5001c81ae689c02f352415bccfb2852bd
Viktor Nikolaiev <destel@users.noreply.github.com>
2024-03-21 01:56:41 +0200

    More documentation (#10)

--- examples/weather/main.go
+++ examples/weather/main.go
@@ -11,25 +11,25 @@ import (
 )
 
 type Measurement struct {
-	Date     time.Time
-	Temp     float64
-	Movement float64
+	Date   time.Time
+	Temp   float64
+	Change float64
 }
 
 func main() {
 	endDate := time.Now()
 	startDate := endDate.AddDate(0, 0, -30)
 
-	err := printTemperatureMovements(context.Background(), "New York", startDate, endDate)
+	err := printTemperatureChanges(context.Background(), "New York", startDate, endDate)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 }
 
-// printTemperatureMovements orchestrates a pipeline that fetches temperature measurements for a given city and
-// prints the daily temperature movements. Measurements are fetched concurrently, but the movements are calculated
+// printTemperatureChanges orchestrates a pipeline that fetches temperature measurements for a given city and
+// prints the daily temperature changes. Measurements are fetched concurrently, but the changes are calculated
 // in order, using a single goroutine.
-func printTemperatureMovements(ctx context.Context, city string, startDate, endDate time.Time) error {
+func printTemperatureChanges(ctx context.Context, city string, startDate, endDate time.Time) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel() // In case of error, this ensures all pending operations are canceled
 
@@ -48,17 +48,17 @@ func printTemperatureMovements(ctx context.Context, city string, startDate, endD
 		return Measurement{Date: date, Temp: temp}, err
 	})
 
-	// Calculate the temperature movements. Use a single goroutine
+	// Calculate the temperature changes. Use a single goroutine
 	prev := Measurement{Temp: math.NaN()}
 	measurements = rill.OrderedMap(measurements, 1, func(m Measurement) (Measurement, error) {
-		m.Movement = m.Temp - prev.Temp
+		m.Change = m.Temp - prev.Temp
 		prev = m
 		return m, nil
 	})
 
-	// Iterate over the measurements and print the movements
+	// Iterate over the measurements and print the results
 	err := rill.ForEach(measurements, 1, func(m Measurement) error {
-		fmt.Printf("%s: %.1f°C (movement %+.1f°C)\n", m.Date.Format("2006-01-02"), m.Temp, m.Movement)
+		fmt.Printf("%s: %.1f°C (change %+.1f°C)\n", m.Date.Format("2006-01-02"), m.Temp, m.Change)
 		prev = m
 		return nil
 	})
