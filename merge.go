e93bb72ef408ac042f2c5c4991ca562d718ff154
destel <destel.od@gmail.com>
2024-03-20 23:21:45 +0200

    Changed Split2 function signature

--- merge.go
+++ merge.go
@@ -13,42 +13,47 @@ func Merge[A any](ins ...<-chan A) <-chan A {
 }
 
 // Split2 divides the input channel into two output channels based on the discriminator function f, using n goroutines for concurrency.
-// The function f takes an item from the input and decides which output channel (out0 or out1) it should go to by returning 0 or 1, respectively.
-// Return values other than 0 or 1 lead to the item being discarded.
+// The function f takes an item from the input and decides which output channel (outTrue or outFalse) it should go to by returning a boolean.
 // If an error is encountered, either from the function f itself or from upstream it is intentionally sent
 // to one of the output channels in a non-deterministic manner.
 // The output order is not guaranteed: results are written to the outputs as soon as they're ready.
 // Use [OrderedSplit2] to preserve the input order.
-func Split2[A any](in <-chan Try[A], n int, f func(A) (int, error)) (out0 <-chan Try[A], out1 <-chan Try[A]) {
+func Split2[A any](in <-chan Try[A], n int, f func(A) (bool, error)) (outTrue <-chan Try[A], outFalse <-chan Try[A]) {
 	outs := core.MapAndSplit(in, 2, n, func(a Try[A]) (Try[A], int) {
 		if a.Error != nil {
 			return a, rand.Int() & 1
 		}
 
-		i, err := f(a.Value)
-		if err != nil {
+		putToTrue, err := f(a.Value)
+		switch {
+		case err != nil:
 			return Try[A]{Error: err}, rand.Int() & 1
+		case putToTrue:
+			return a, 0
+		default:
+			return a, 1
 		}
-
-		return a, i
 	})
 
 	return outs[0], outs[1]
 }
 
 // OrderedSplit2 is similar to [Split2], but it guarantees that the order of the outputs matches the order of the input.
-func OrderedSplit2[A any](in <-chan Try[A], n int, f func(A) (int, error)) (out0 <-chan Try[A], out1 <-chan Try[A]) {
+func OrderedSplit2[A any](in <-chan Try[A], n int, f func(A) (bool, error)) (outTrue <-chan Try[A], outFalse <-chan Try[A]) {
 	outs := core.OrderedMapAndSplit(in, 2, n, func(a Try[A]) (Try[A], int) {
 		if a.Error != nil {
 			return a, rand.Int() & 1
 		}
 
-		i, err := f(a.Value)
-		if err != nil {
+		putToTrue, err := f(a.Value)
+		switch {
+		case err != nil:
 			return Try[A]{Error: err}, rand.Int() & 1
+		case putToTrue:
+			return a, 0
+		default:
+			return a, 1
 		}
-
-		return a, i
 	})
 
 	return outs[0], outs[1]
--- merge_test.go
+++ merge_test.go
@@ -12,121 +12,123 @@ func TestMerge(t *testing.T) {
 	Merge[int](nil)
 }
 
-func universalSplit[A any](ord bool, in <-chan Try[A], numOuts int, n int, f func(A) (int, error)) []<-chan Try[A] {
-	switch {
-	case !ord && numOuts == 2:
-		out0, out1 := Split2(in, n, f)
-		return []<-chan Try[A]{out0, out1}
-	case ord && numOuts == 2:
-		out0, out1 := OrderedSplit2(in, n, f)
-		return []<-chan Try[A]{out0, out1}
-	default:
-		panic("unsupported")
+func universalSplit2[A any](ord bool, in <-chan Try[A], n int, f func(A) (bool, error)) (outTrue <-chan Try[A], outFalse <-chan Try[A]) {
+	if ord {
+		return OrderedSplit2(in, n, f)
 	}
+	return Split2(in, n, f)
 }
 
 func TestSplit2(t *testing.T) {
 	th.TestBothOrderings(t, func(t *testing.T, ord bool) {
 		for _, n := range []int{1, 5} {
-			for _, numOuts := range []int{2} {
+			t.Run(th.Name("nil", n), func(t *testing.T) {
+				outTrue, outFalse := universalSplit2(ord, nil, n, func(string) (bool, error) { return true, nil })
+				th.ExpectValue(t, outTrue, nil)
+				th.ExpectValue(t, outFalse, nil)
+			})
 
-				t.Run(th.Name("nil", numOuts, n), func(t *testing.T) {
-					outs := universalSplit(ord, nil, numOuts, n, func(string) (int, error) { return 0, nil })
-					th.ExpectSlice(t, outs, make([]<-chan Try[string], numOuts))
+			t.Run(th.Name("correctness", n), func(t *testing.T) {
+				// idea: split input into 4 groups
+				// - first 2 groups are sent into corresponding outputs
+				// - 3rd would cause error during splitting
+				// - 4th would be errors even before splitting
+
+				in := WrapChan(th.FromRange(0, 20*4), nil)
+				in = OrderedMap(in, 1, func(x int) (int, error) {
+					if x%4 == 3 {
+						return 0, fmt.Errorf("err%03d", x)
+					}
+					return x, nil
 				})
 
-				t.Run(th.Name("correctness", numOuts, n), func(t *testing.T) {
-					// idea: split input into numOuts+3 groups
-					// - first numOuts groups are sent into corresponding outputs
-					// - next group is filtered out
-					// - next group would cause error during splitting
-					// - next group would be errors even before splitting
-
-					in := WrapChan(th.FromRange(0, 20*(numOuts+3)), nil)
-					in = OrderedMap(in, 1, func(x int) (int, error) {
-						outID := x % (numOuts + 3)
-						if outID == numOuts+2 {
-							return x, fmt.Errorf("err%03d", x)
-						}
-						return x, nil
-					})
-
-					outs := universalSplit(ord, in, numOuts, n, func(x int) (int, error) {
-						outID := x % (numOuts + 3)
-						if outID == numOuts+1 {
-							return 0, fmt.Errorf("err%03d", x)
-						}
-						return outID, nil
-					})
-
-					outSlices := make([][]int, numOuts)
-					errSlices := make([][]string, numOuts)
-
-					th.DoConcurrentlyN(numOuts, func(i int) {
-						outSlices[i], errSlices[i] = toSliceAndErrors(outs[i])
-					})
-
-					expectedOutSlices := make([][]int, numOuts)
-					expectedAllErrsSlice := make([]string, 0, numOuts)
-
-					for i := 0; i < 20*(numOuts+3); i++ {
-						outID := i % (numOuts + 3)
-						if outID > numOuts {
-							expectedAllErrsSlice = append(expectedAllErrsSlice, fmt.Sprintf("err%03d", i))
-						}
-						if outID < numOuts {
-							expectedOutSlices[outID] = append(expectedOutSlices[outID], i)
-						}
-					}
-
-					var allErrsSlice []string
-					for _, errSlice := range errSlices {
-						allErrsSlice = append(allErrsSlice, errSlice...)
-						if len(errSlice) == 0 {
-							t.Errorf("expected at least one error in each channel")
-						}
-					}
-
-					th.Sort(allErrsSlice)
-					th.ExpectSlice(t, allErrsSlice, expectedAllErrsSlice)
-
-					for i := range outSlices {
-						th.Sort(outSlices[i])
-						th.ExpectSlice(t, outSlices[i], expectedOutSlices[i])
-					}
-
-				})
-
-				t.Run(th.Name("ordering", numOuts, n), func(t *testing.T) {
-					in := WrapChan(th.FromRange(0, 10000*(numOuts+1)), nil)
-
-					outs := universalSplit(ord, in, numOuts, n, func(x int) (int, error) {
-						outID := x % (numOuts + 1)
-						if outID == numOuts {
-							return 0, fmt.Errorf("err%06d", x)
-						}
-						return outID, nil
-					})
-
-					outSlices := make([][]int, numOuts)
-					errSlices := make([][]string, numOuts)
-
-					th.DoConcurrentlyN(len(outs), func(i int) {
-						outSlices[i], errSlices[i] = toSliceAndErrors(outs[i])
-					})
-
-					for i := range outSlices {
-						if ord || n == 1 {
-							th.ExpectSorted(t, outSlices[i])
-							th.ExpectSorted(t, errSlices[i])
-						} else {
-							th.ExpectUnsorted(t, outSlices[i])
-							th.ExpectUnsorted(t, errSlices[i])
-						}
+				outTrue, outFalse := universalSplit2(ord, in, n, func(x int) (bool, error) {
+					switch x % 4 {
+					case 0:
+						return true, nil
+					case 1:
+						return false, nil
+					case 2:
+						return true, fmt.Errorf("err%03d", x)
+					default:
+						return true, nil // this should not be called
 					}
 				})
 
-			}
+				var outSliceTrue, outSliceFalse []int
+				var errSliceTrue, errSliceFalse []string
+
+				th.DoConcurrently(
+					func() { outSliceTrue, errSliceTrue = toSliceAndErrors(outTrue) },
+					func() { outSliceFalse, errSliceFalse = toSliceAndErrors(outFalse) },
+				)
+
+				var expectedOutSliceTrue, expectedOutSliceFalse []int
+				var expectedAllErrsSlice []string
+
+				for i := 0; i < 20*4; i++ {
+					switch i % 4 {
+					case 0:
+						expectedOutSliceTrue = append(expectedOutSliceTrue, i)
+					case 1:
+						expectedOutSliceFalse = append(expectedOutSliceFalse, i)
+					default:
+						expectedAllErrsSlice = append(expectedAllErrsSlice, fmt.Sprintf("err%03d", i))
+					}
+				}
+
+				var allErrsSlice []string
+				allErrsSlice = append(allErrsSlice, errSliceTrue...)
+				allErrsSlice = append(allErrsSlice, errSliceFalse...)
+
+				if len(errSliceTrue) == 0 || len(errSliceFalse) == 0 {
+					t.Errorf("expected at least one error in each output")
+				}
+
+				th.Sort(outSliceTrue)
+				th.Sort(outSliceFalse)
+				th.Sort(allErrsSlice)
+
+				th.ExpectSlice(t, outSliceTrue, expectedOutSliceTrue)
+				th.ExpectSlice(t, outSliceFalse, expectedOutSliceFalse)
+				th.ExpectSlice(t, allErrsSlice, expectedAllErrsSlice)
+			})
+
+			t.Run(th.Name("ordering", n), func(t *testing.T) {
+				in := WrapChan(th.FromRange(0, 10000*4), nil)
+
+				outTrue, outFalse := universalSplit2(ord, in, n, func(x int) (bool, error) {
+					switch x % 3 {
+					case 0:
+						return true, nil
+					case 1:
+						return false, nil
+					default:
+						return true, fmt.Errorf("err%06d", x)
+					}
+				})
+
+				var outSliceTrue, outSliceFalse []int
+				var errSliceTrue, errSliceFalse []string
+
+				th.DoConcurrently(
+					func() { outSliceTrue, errSliceTrue = toSliceAndErrors(outTrue) },
+					func() { outSliceFalse, errSliceFalse = toSliceAndErrors(outFalse) },
+				)
+
+				if ord || n == 1 {
+					th.ExpectSorted(t, outSliceTrue)
+					th.ExpectSorted(t, outSliceFalse)
+					th.ExpectSorted(t, errSliceTrue)
+					th.ExpectSorted(t, errSliceFalse)
+				} else {
+					th.ExpectUnsorted(t, outSliceTrue)
+					th.ExpectUnsorted(t, outSliceFalse)
+					th.ExpectUnsorted(t, errSliceTrue)
+					th.ExpectUnsorted(t, errSliceFalse)
+				}
+			})
+
 		}
 	})
 }
