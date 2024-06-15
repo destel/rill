824bac7ad84eff7a21683b5d11af5ae7d69dd02c
destel <destel.od@gmail.com>
2024-06-15 10:29:33 +0300

    Moved ForEach to consume.go

--- consume.go
+++ consume.go
@@ -1,6 +1,59 @@
 package rill
 
-import "errors"
+import (
+	"errors"
+	"sync"
+
+	"github.com/destel/rill/internal/core"
+)
+
+// ForEach applies a function f to each item in an input channel using n goroutines for parallel processing. The function
+// blocks until all items are processed or an error is encountered, either from the function f itself or from upstream.
+// In case of an error leading to early termination, ForEach ensures the input channel is drained to avoid goroutine leaks,
+// making it safe for use in environments where cleanup is crucial. The function returns the first encountered error, or nil
+// if all items were processed successfully.
+// While this function does not guarantee the order of item processing due to its concurrent nature,
+// using n = 1 results in sequential processing, as in a simple for-range loop.
+func ForEach[A any](in <-chan Try[A], n int, f func(A) error) error {
+	if n == 1 {
+		for a := range in {
+			err := a.Error
+			if err == nil {
+				err = f(a.Value)
+			}
+
+			if err != nil {
+				DrainNB(in)
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	var retErr error
+	var once sync.Once
+
+	in, earlyExit := core.Breakable(in)
+	done := make(chan struct{})
+
+	core.Loop(in, done, n, func(a Try[A]) {
+		err := a.Error
+		if err == nil {
+			err = f(a.Value)
+		}
+
+		if err != nil {
+			earlyExit()
+			once.Do(func() {
+				retErr = err
+			})
+		}
+	})
+
+	<-done
+	return retErr
+}
 
 // Any checks if there is an item in the input channel that satisfies the condition f.
 // This function uses n goroutines for concurrency. It blocks execution until either:
