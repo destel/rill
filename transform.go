f7f8845883954601a1243feddff1ae848c132299
destel <destel.od@gmail.com>
2024-07-21 17:12:06 +0300

    Code and tests

--- transform.go
+++ transform.go
@@ -80,6 +80,45 @@ func OrderedFilter[A any](in <-chan Try[A], n int, f func(A) (bool, error)) <-ch
 	})
 }
 
+// FilterMap takes a stream of items of type A, applies a function f that can filter and transform them into items of type B.
+// Returns a new stream of transformed items that passed the filter. This operation is equivalent to a
+// [Filter] followed by a [Map].
+//
+// This is a non-blocking unordered function that processes items concurrently using n goroutines.
+// An ordered version of this function, [OrderedFilterMap], is also available.
+//
+// See the package documentation for more information on non-blocking unordered functions and error handling.
+func FilterMap[A, B any](in <-chan Try[A], n int, f func(A) (B, bool, error)) <-chan Try[B] {
+	return core.FilterMap(in, n, func(a Try[A]) (Try[B], bool) {
+		if a.Error != nil {
+			return Try[B]{Error: a.Error}, true
+		}
+
+		b, keep, err := f(a.Value)
+		if err != nil {
+			return Try[B]{Error: err}, true
+		}
+
+		return Try[B]{Value: b}, keep
+	})
+}
+
+// OrderedFilterMap is the ordered version of [FilterMap].
+func OrderedFilterMap[A, B any](in <-chan Try[A], n int, f func(A) (B, bool, error)) <-chan Try[B] {
+	return core.OrderedFilterMap(in, n, func(a Try[A]) (Try[B], bool) {
+		if a.Error != nil {
+			return Try[B]{Error: a.Error}, true
+		}
+
+		b, keep, err := f(a.Value)
+		if err != nil {
+			return Try[B]{Error: err}, true
+		}
+
+		return Try[B]{Value: b}, keep
+	})
+}
+
 // FlatMap takes a stream of items of type A and transforms each item into a new sub-stream of items of type B using a function f.
 // Those sub-streams are then flattened into a single output stream, which is returned.
 //
