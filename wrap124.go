8dea8e37d1a94ab594c5a794b31f0a502dff16b0
destel <destel.od@gmail.com>
2025-02-02 17:26:06 +0200

    Add Stream[T] type alias

---
+++ wrap124.go
@@ -0,0 +1,19 @@
+//go:build go1.24
+
+package rill
+
+// Stream is a type alias for a channel of Try containers.
+// This alias is optional, but it can make the code more readable.
+//
+// Before:
+//
+//	func StreamUsers() <-chan rill.Try[*User] {
+//		...
+//	}
+//
+// After:
+//
+//	func StreamUsers() rill.Stream[*User] {
+//		...
+//	}
+type Stream[T any] = <-chan Try[T]
