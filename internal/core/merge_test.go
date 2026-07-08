d2ff3adda9f468a2f5805a0d598df7d3c51d7614
destel <destel.od@gmail.com>
2026-07-09 00:08:27 +0300

    Rework TestMerge: add per-input independence, drop redundant unclosed variant

--- internal/core/merge_test.go
+++ internal/core/merge_test.go
@@ -1,8 +1,8 @@
 package core
 
 import (
-	"fmt"
 	"testing"
+	"time"
 
 	"github.com/destel/rill/internal/th"
 )
@@ -13,67 +13,70 @@ func TestMerge(t *testing.T) {
 		th.ExpectValue(t, out, nil)
 	})
 
-	makeSubChan := func(chanID int) <-chan string {
-		ch := make(chan string)
-		go func() {
-			defer close(ch)
-			ch <- fmt.Sprintf("%03dA", chanID)
-			ch <- fmt.Sprintf("%03dB", chanID)
-			ch <- fmt.Sprintf("%03dC", chanID)
-			ch <- fmt.Sprintf("%03dD", chanID)
-		}()
-		return ch
-	}
-
-	appendExpected := func(acc []string, chanID int) []string {
-		return append(acc,
-			fmt.Sprintf("%03dA", chanID),
-			fmt.Sprintf("%03dB", chanID),
-			fmt.Sprintf("%03dC", chanID),
-			fmt.Sprintf("%03dD", chanID),
-		)
-	}
-
 	for _, numChans := range []int{1, 3, 5, 10} {
 		th.RunSynctest(t, th.Name("correctness", numChans), func(t *testing.T) {
-			ins := make([]<-chan string, numChans)
+			ins := make([]<-chan int, numChans)
 
 			for i := range numChans {
-				ins[i] = makeSubChan(i)
+				ins[i] = th.FromRange(20*i, 20*(i+1))
 			}
 
 			out := Merge(ins...)
 			outSlice := th.ToSlice(out)
 
-			var expectedSlice []string
-			for i := range numChans {
-				expectedSlice = appendExpected(expectedSlice, i)
+			var expectedSlice []int
+			for i := range 20 * numChans {
+				expectedSlice = append(expectedSlice, i)
 			}
 
 			th.ExpectElementsMatch(t, outSlice, expectedSlice)
 		})
 
-		t.Run(th.Name("nil hang", numChans), func(t *testing.T) {
-			var outSlice []string
+		th.RunSynctest(t, th.Name("per input independence", numChans), func(t *testing.T) {
+			ins := make([]<-chan int, numChans)
+			insWritable := make([]chan int, numChans)
+			for i := range numChans {
+				ch := make(chan int)
+				insWritable[i] = ch
+				ins[i] = ch
+			}
 
+			go func() {
+				// write to the channels in a round-robin fashion
+				for i := 0; i < 100; i++ {
+					th.SimulateWork(1*time.Second, 2*time.Second)
+					insWritable[i%len(insWritable)] <- i
+				}
+
+				for _, ch := range insWritable {
+					close(ch)
+				}
+			}()
+
+			out := Merge(ins...)
+			outSlice := th.ToSlice(out)
+
+			var expectedSlice []int
+			for i := 0; i < 100; i++ {
+				expectedSlice = append(expectedSlice, i)
+			}
+
+			th.ExpectSlice(t, outSlice, expectedSlice) // exact order
+		})
+
+		t.Run(th.Name("nil hang", numChans), func(t *testing.T) {
 			th.ExpectBlock(t, func(t *testing.T) {
-				ins := make([]<-chan string, numChans)
-				for i := 0; i < numChans-1; i++ {
-					ins[i] = makeSubChan(i)
+				ins := make([]<-chan int, numChans)
+				for i := range numChans {
+					if i == len(ins)/2 {
+						continue // leave the middle channel nil
+					}
+					ins[i] = th.FromRange(20*i, 20*(i+1))
 				}
 
 				out := Merge(ins...)
-				for x := range out {
-					outSlice = append(outSlice, x)
-				}
+				Drain(out)
 			})
-
-			var expectedSlice []string
-			for i := 0; i < numChans-1; i++ {
-				expectedSlice = appendExpected(expectedSlice, i)
-			}
-
-			th.ExpectElementsMatch(t, outSlice, expectedSlice)
 		})
 	}
 }
